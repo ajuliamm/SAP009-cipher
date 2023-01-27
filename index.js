2b614b9eef6e962b811f11007d1572ceaafbba81
ajuliamm <ajuliammelo@gmail.com>
2023-01-26 22:00:08 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,44 @@
+import cipher from './cipher.js';
+
+const textareaEncrypt = document.getElementById('encrypt-area');
+const textareaDecrypt = document.getElementById('decrypt-area');
+const keyEncrypt = document.getElementById('key-encrypt')
+const keyDecrypt = document.getElementById('key-decrypt')
+const encryptButton = document.getElementById('encrypt');
+const decryptButton = document.getElementById('decrypt');
+
+//Transformar as letras em maiúsculas
+textareaEncrypt.addEventListener('keyup',(valorTextarea)=>{
+  const letter=valorTextarea.target;
+  letter.value= letter.value.toUpperCase();
+})
+textareaDecrypt.addEventListener('keyup',(valorTextarea)=>{
+  const letter = valorTextarea.target;
+  letter.value= letter.value.toUpperCase();
+})
+
+//BOTÃO PARA CRIPTOGRAFAR
+encryptButton.addEventListener('click', encryptText);
+function encryptText(){
+  if(textareaEncrypt.value ==="" ||keyEncrypt.value === ""){
+    alert('Preencha todos os campos para codificar!')
+  }else{
+    const key = Number(keyEncrypt.value);
+    const message = textareaEncrypt.value;
+    textareaDecrypt.value = cipher.encode(key,message)
+  }
+
+}
+
+
+//BOTÃO PARA DESCRIPTOGRAFAR
+decryptButton.addEventListener('click', decryptText)
+function decryptText(){
+  if(textareaDecrypt.value ==="" ||keyDecrypt.value === ""){
+    alert('Preencha todos os campos para decodificar!')
+  }else{
+    const key = Number(keyDecrypt.value)
+    const message = textareaDecrypt.value
+    textareaEncrypt.value = cipher.decode(key,message)
+  }
+}
